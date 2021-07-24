3819930ce7c25cc71ad240c4a7269814b27a6713
Jordan <jordanashment@gmail.com>
2021-07-24 13:56:28 -0600

    Playing with list views

--- components/Monsters/Monsters.js
+++ components/Monsters/Monsters.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ImageBackground, Image, Text, View } from 'react-native'
+import { ImageBackground, Image, FlatList, Text, View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {Card} from 'react-native-elements'
 import styles from '../../styles/mainStyles'
@@ -11,10 +11,33 @@ const Monsters = () => {
             <View style={styles.container}>
                 <ImageBackground source={require('../../assets/cat.jpg')} resizeMode={'cover'} style={styles.backgroundImage}>
                     <View style={{ position: 'absolute', bottom: 20, width: '100%', height: '50%' }}>
-                        <Card>
+                        <FlatList
+                            data={[
+                                {
+                                    key: 'wolf',
+                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                },
+                                {
+                                    key: 'bear',
+                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                },
+                                {
+                                    key: 'dragon',
+                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                }
+                            ]}
+                            renderItem={(item) => {
+                                return (
+                                    <Card style={{height: '200px'}}>
+                                        <Text>{item.key}</Text>
+                                        {/* <Image source={{uri: item.uri}} style={{ alignSelf: 'center', width: '100%', height: '100%' }}/> */}
+                                    </Card>
+                                )}}
+                        />
+                        {/* <Card>
                             <Image source={{uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'}} style={{ alignSelf: 'center', width: '100%', height: '100%' }}/>
                             <Text>Monsters</Text>
-                        </Card>
+                        </Card> */}
                     </View>
                 </ImageBackground>
             </View>
