739a1c0ab996161409adde0329843eb8b138337f
Jordan <jordanashment@gmail.com>
2021-08-04 21:33:50 -0600

    Pics finally working!

--- components/Monsters/Monsters.js
+++ components/Monsters/Monsters.js
@@ -10,49 +10,42 @@ const Monsters = () => {
         <SafeAreaView style={styles.container}>
             <ImageBackground source={require('../../assets/cat.jpg')} resizeMode={'cover'} style={styles.backgroundImage}>
             <View style={styles.container}>
-                
-                    {/* <View> */}
                     <FlatList
-                            style={{ flex: 1 }}
                             inverted
-                            // keyExtriactor={(time, index) => index.toString()}
                             data={[
                                 {
-                                    key: 'wolf',
-                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                    key: 'Werewolf',
+                                    uri: 'https://cdn.pixabay.com/photo/2018/04/09/22/17/mammal-3305724_1280.jpg'
                                 },
                                 {
-                                    key: 'bear',
-                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                    key: 'Shapeshifter',
+                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg'
                                 },
                                 {
-                                    key: 'dragon',
-                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                    key: 'Dragon',
+                                    uri: 'https://cdn.pixabay.com/photo/2018/01/25/13/25/outdoors-3106126_1280.jpg'
                                 },
                                 {
-                                    key: 'vampire',
-                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                                    key: 'Vampire',
+                                    uri: 'https://images.pexels.com/photos/6394135/pexels-photo-6394135.jpeg'
                                 }
                             ]}
-                            renderItem={(item) => {
+                            renderItem = {({item}) => {
                                 return (
-                                    <View>
-                                        <View style={styles.container}>
-                                    <Card style={{height: '500px'}}>
-                                        <Text style={{
-                                            paddingVertical: 10,
-                                            fontSize: 15,
-                                            paddingStart: 5,
-                                            paddingEnd: 16,
-                                            color: 'black'
-                                        }}>{item.key}</Text>
+                                        <View>
+                                            <Card style={{height: '500px'}}>
+                                                <Text style={{
+                                                    paddingVertical: 10,
+                                                    fontSize: 15,
+                                                    paddingStart: 5,
+                                                    paddingEnd: 16,
+                                                    color: 'black'
+                                                }}>{item.key}</Text>
+                                                <Image source={{uri: item.uri}} style={{height: 300, width: 300}}/>
                                             </Card>
-                                            </View>
-                                    </View>
+                                        </View>
                                 )}}
                         />
-                    {/* </View> */}
-                
             </View>
             </ImageBackground>
         </SafeAreaView>
