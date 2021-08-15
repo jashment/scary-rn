44204a366700e0c795b51cca5c409a000dc73c62
Jordan <jordanashment@gmail.com>
2021-08-15 15:33:22 -0600

    fixing backend vars

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,16 +1,20 @@
 import axios from 'axios'
-import { REACT_APP_BACKEND_URL } from '@env'
+import { REACT_APP_BACKEND_URL, NODE_ENV } from '@env'
 import { Platform } from 'react-native'
 
 
 const chooseUrl = () => {
     let backendUrl
-    if (Platform.OS === 'android') {
-        backendUrl = 'http://10.0.2.2:3000'
-    } else if (Platform.OS === 'ios') {
-        backendUrl = 'http://localhost:3000'
-    } else if (Platform.OS === 'web') {
-        backendUrl = 'http://localhost:3000'
+    if (NODE_ENV === 'development') {
+        if (Platform.OS === 'android') {
+            backendUrl = 'http://10.0.2.2:3000'
+        } else if (Platform.OS === 'ios') {
+            backendUrl = 'http://localhost:3000'
+        } else if (Platform.OS === 'web') {
+            backendUrl = 'http://localhost:3000'
+        }
+    } else {
+        backendUrl = REACT_APP_BACKEND_URL
     }
     return backendUrl
 }
