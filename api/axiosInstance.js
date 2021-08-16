c5311054620ba3722bfcbe3f3ffc84e02ee87adb
Jordan <jordanashment@gmail.com>
2021-08-15 20:12:50 -0600

    env var play

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -7,8 +7,10 @@ const chooseUrl = () => {
     let backendUrl
     console.log(NODE_ENV)
     if (NODE_ENV === 'production') {
+        console.log('prod')
         backendUrl = REACT_APP_BACKEND_URL
     } else if (NODE_ENV === 'development') {
+        console.log('dev')
         if (Platform.OS === 'android') {
             backendUrl = 'http://10.0.2.2:3000'
         } else if (Platform.OS === 'ios') {
