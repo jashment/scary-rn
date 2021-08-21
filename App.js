c3ac2816415bfd8a570b9120059427d03553b9e0
Jordan <jordanashment@gmail.com>
2021-08-21 15:17:13 -0600

    Modify axios instance

--- App.js
+++ App.js
@@ -5,16 +5,21 @@ import Monsters from './components/Monsters/Monsters'
 import Home from './components/Home/Home'
 import { createStackNavigator } from '@react-navigation/stack';
 import styles from './styles/mainStyles'
+import { useFonts, Rye_400Regular } from '@expo-google-fonts/rye';
 
 
 const Stack = createStackNavigator()
 
 export default function App() {
+    useFonts({
+        Rye_400Regular,
+    });
+
   return (
     <SafeAreaProvider style={styles.container}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} options={{ headerTransparent: true, headerTitleAlign: 'center', headerStyle: {height: 75}, headerTitleStyle: {color: 'white'}}} />
+        <Stack.Navigator initialRouteName="Tome of Terror" screenOptions={{gestureEnabled: true}}>
+          <Stack.Screen name="Tome of Terror" component={Home} options={{ headerTransparent: true, headerTitleAlign: 'center', headerStyle: {height: 75}, headerTitleStyle: {color: 'white', fontFamily: 'Rye_400Regular'}}} />
           <Stack.Screen name="Monsters" component={Monsters} options={{headerTransparent: true, headerTitleAlign: 'center', headerStyle: {height: 75}, headerTitleStyle: {color: 'white'}}} />
         </Stack.Navigator>
       </NavigationContainer>
--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,25 +1,7 @@
 import axios from 'axios'
-import { REACT_APP_BACKEND_URL, NODE_ENV } from '@env'
-import { Platform } from 'react-native'
+import { REACT_APP_BACKEND_URL } from '@env'
 
-
-const chooseUrl = () => {
-    let backendUrl
-    if (NODE_ENV === 'production') {
-        return REACT_APP_BACKEND_URL
-    } else if (NODE_ENV === 'development') {
-        if (Platform.OS === 'android') {
-            backendUrl = 'http://10.0.2.2:3000'
-        } else if (Platform.OS === 'ios') {
-            backendUrl = 'http://localhost:3000'
-        } else if (Platform.OS === 'web') {
-            backendUrl = 'http://localhost:3000'
-        }
-        return backendUrl
-    }
-}
 
 export const axiosInstance = axios.create({
-    baseURL: chooseUrl(),
-    // headers: {'X-Requested-With': 'XMLHttpRequest'},
+    baseURL: REACT_APP_BACKEND_URL,
 })
