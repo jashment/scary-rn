e0066de1fed23220bc6b02ded704d9a5a0e3609e
Jordan <jordanashment@gmail.com>
2021-08-21 21:04:07 -0600

    Adding a placeholder icon

--- components/Monsters/Monsters.js
+++ components/Monsters/Monsters.js
@@ -35,9 +35,9 @@ const Monsters = () => {
                         keyExtractor = {key => key.id}
                         renderItem = {({item}) => 
                             (
-                                <Card containerStyle={{backgroundColor: 'rgba(128, 128, 128, 0.3)', borderColor: 'rgba(128, 128, 128, 0.3)', justifyContent: 'center'}} wrapperStyle={{alignItems: 'center'}}>
+                                <Card containerStyle={{backgroundColor: 'rgba(128, 128, 128, 0.5)', borderColor: 'rgba(128, 128, 128, 0.3)', justifyContent: 'center'}} wrapperStyle={{alignItems: 'center'}}>
                                     <Card.Title style={{ fontFamily: 'Rye_400Regular', fontSize: 32, color: 'rgba(255, 255, 255, 1)'}}>{item.name}</Card.Title>
-                                    <Card.Image source={{ uri: item.url }} style={{ height: 300, width: 300, borderRadius: 100 }} />
+                                    <Card.Image source={item.url ? { uri: item.url } : require('../../assets/ghost_skull.png')} style={{ height: 300, width: 300, borderRadius: 100 }} />
                                     <Card.Divider/>
                                     <Text style={{color: 'rgba(255, 255, 255, 1)'}}>{item.description}</Text>
                                 </Card>
--- styles/mainStyles.js
+++ styles/mainStyles.js
@@ -4,25 +4,13 @@ import Constants from "expo-constants";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // paddingTop: 32
-    // marginTop: Constants.statusBarHeight,
-    // textAlign: 'center'
   },
   backgroundImage: {
     flex: 1,
-    // justifyContent: "center",
-    // marginTop: 10
   },
   text: {
     color: 'white',
-    // margin: 'auto',
   },
-  btn: {
-    // backgroundColor: 'red'
-    // alignSelf: 'flex-start'
-    // flex: 1,
-    // justifyContent: 'flex-start'
-  }
 });
 
 export default styles
